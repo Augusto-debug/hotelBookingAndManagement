ec8c8e23a4d977e05b7734d074afbbce75df884a
Augusto <augustocesarrezende64@gmail.com>
2024-11-29 14:53:10 -0300

    métodos abstratos criados para o BookingRepository

--- backend/src/main/java/com/augusto/backend/repo/BookingRepository.java
+++ backend/src/main/java/com/augusto/backend/repo/BookingRepository.java
@@ -1,4 +1,13 @@
 package com.augusto.backend.repo;
 
-public interface BookingRepository {
+import com.augusto.backend.entity.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface BookingRepository extends JpaRepository<Booking, Long> {
+    List<Booking> findByRoomId(Long roomId);
+    List<Booking> findByBookingConfirmation(String bookingConfirmation);
+    List<Booking> findByUserId(Long userId);
 }
