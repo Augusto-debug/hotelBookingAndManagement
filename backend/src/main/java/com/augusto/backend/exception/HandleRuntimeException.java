856b9b8def1d9b6ceca7de5396437ae25d71a839
Augusto <augustocesarrezende64@gmail.com>
2024-12-09 14:12:45 -0300

    classe de excessão criada e classe de serviço para identificar login pelo usuario criada

--- backend/src/main/java/com/augusto/backend/exception/HandleRuntimeException.java
+++ backend/src/main/java/com/augusto/backend/exception/HandleRuntimeException.java
@@ -1,8 +1,11 @@
 package com.augusto.backend.exception;
 
+import java.io.Serial;
+
 public class HandleRuntimeException extends RuntimeException {
+    @Serial
     private static final long serialVersionUID = 1L;
-   public HandleRuntimeException(String message) {
+    public HandleRuntimeException(String message) {
         super(message);
     }
 }
